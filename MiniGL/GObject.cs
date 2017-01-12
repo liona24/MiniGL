ac82c7b6d2fa98d3be992fc364b43deb64cbebcd
Lion Ackermann <ackerm.lion@hotmail.de>
2017-01-12 16:12:53 +0100

    GObjects now use a factory for creation

--- MiniGL/GObject.cs
+++ MiniGL/GObject.cs
@@ -18,23 +18,21 @@ namespace MiniGL
         public int Length { get { return points.Length; } }
         public Vec3 this[int i] { get { return points[i]; } set { points[i] = value; } }
         public TMaker2D TMaker { get { return tmaker; } set { tmaker = value; } }
+        public Rect Boundaries { get { return boundaries; } }
 
-        public GObject2D(Vec3 p1, Vec3 p2, Vec3 p3, TMaker2D tmaker)
+        protected GObject2D(Vec3 p1, Vec3 p2, Vec3 p3, TMaker2D tmaker)
         {
             this.tmaker = tmaker;
             points = new Vec3[] { p1, p2, p3 };
             UpdateBoundaries();
         }
-        public GObject2D(Vec3 p1, Vec3 p2, TMaker2D tmaker)
+        protected GObject2D(Vec3 p1, Vec3 p2, TMaker2D tmaker)
         {
             this.tmaker = tmaker;
             points = new Vec3[] { p1, p2 };
             UpdateBoundaries();
         }
-
-        public Rect Boundaries { get { return boundaries; } }
-
-        //only used for xml serialization
+        
         public GObject2D()
         { }
 
@@ -114,6 +112,27 @@ namespace MiniGL
         {
 
         }
+
+        public class GObject2DFactory
+        {
+            public virtual GObject2D Create(Vec3 p1, Vec3 p2, Vec3 p3, TMaker2D tmaker)
+            {
+                return new GObject2D(p1, p2, p3, tmaker);
+            }
+            public virtual GObject2D Create(Vec3 p1, Vec3 p2, TMaker2D tmaker)
+            {
+                return new GObject2D(p1, p2, tmaker);
+            }
+            public virtual GObject2D Create(I3Dimensional p1, I3Dimensional p2, I3Dimensional p3, TMaker2D tmaker)
+            {
+                return new GObject2D(new Vec3(p1.X, p1.Y, p1.Z), new Vec3(p2.X, p2.Y, p2.Z),
+                                    new Vec3(p3.X, p3.Y, p3.Z), tmaker);
+            }
+            public virtual GObject2D Create(I3Dimensional p1, I3Dimensional p2, TMaker2D tmaker)
+            {
+                return new GObject2D(new Vec3(p1.X, p1.Y, p1.Z), new Vec3(p2.X, p2.Y, p2.Z), tmaker);
+            }
+        }
     }
 
     /// <summary>
@@ -122,8 +141,8 @@ namespace MiniGL
     public class GObject : IXmlSerializable, IHasBoundaries3
     {
         private Vec4[] points;
+        private Cuboid boundaries;
 
-        protected Cuboid boundaries;
         protected TMaker tmaker;
 
         public Cuboid Boundaries {  get { return boundaries; } }
@@ -132,13 +151,13 @@ namespace MiniGL
         public Vec4 this[int i] { get { return points[i]; } set { points[i] = value; } }
         public TMaker TMaker { get { return tmaker; } set { tmaker = value; } }
 
-        public GObject(Vec4 p1, Vec4 p2, Vec4 p3, TMaker tmaker)
+        protected GObject(Vec4 p1, Vec4 p2, Vec4 p3, TMaker tmaker)
         {
             this.tmaker = tmaker;
             points = new Vec4[] { p1, p2, p3 };
             UpdateBoundaries();
         }
-        public GObject(Vec4 p1, Vec4 p2, TMaker tmaker)
+        protected GObject(Vec4 p1, Vec4 p2, TMaker tmaker)
         {
             this.tmaker = tmaker;
             points = new Vec4[] { p1, p2 };
@@ -231,6 +250,29 @@ namespace MiniGL
         protected virtual void readAdditionalXml(XmlReader read)
         {
 
+        }
+
+        public class GObjectFactory
+        {
+            public virtual GObject Create(Vec4 p1, Vec4 p2, TMaker tmaker)
+            {
+                return new GObject(p1, p2, tmaker);
+            }
+            public virtual GObject Create(Vec4 p1, Vec4 p2, Vec4 p3, TMaker tmaker)
+            {
+                return new GObject(p1, p2, p3, tmaker);
+            }
+            public virtual GObject Create(I4Dimensional p1, I4Dimensional p2, TMaker tmaker)
+            {
+                return new GObject(new Vec4(p1.X, p1.Y, p1.Z, p1.W),
+                        new Vec4(p2.X, p2.Y, p2.Z, p2.W), tmaker);
+            }
+            public virtual GObject Create(I4Dimensional p1, I4Dimensional p2, I4Dimensional p3, TMaker tmaker)
+            {
+                return new GObject(new Vec4(p1.X, p1.Y, p1.Z, p1.W),
+                        new Vec4(p2.X, p2.Y, p2.Z, p2.W),
+                        new Vec4(p3.X, p3.Y, p3.Z, p3.W), tmaker);
+            }
         }
     }
 
