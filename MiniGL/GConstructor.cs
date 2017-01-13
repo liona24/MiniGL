1c09ad0192f171579b8cf5efcfebd36557bbab54
Lion Ackermann <ackerm.lion@hotmail.de>
2017-01-14 00:44:33 +0100

    fix errors in GConstructor#

--- MiniGL/GConstructor.cs
+++ MiniGL/GConstructor.cs
@@ -10,101 +10,91 @@ namespace MiniGL
         ///<summary>
         /// Constructs a cube with side length=1 and center laying at (0,0,0)
         ///</summary>
-        public static GObject[] MakeCube(bool fill, TMaker tmaker, GManager manager)
+        public static void MakeCube(bool fill, GManager manager)
         {
-            var res = new GObject[12];
             if (fill)
             {
                 //front surface 
-                res[0] = manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(0.5, 0.5, -0.5, 1), new Vec4(0.5, -0.5, -0.5, 1));
-                res[1] = manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(0.5, -0.5, -0.5, 1), new Vec4(0.5, -0.5, -0.5, 1));
+                manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(0.5, 0.5, -0.5, 1), new Vec4(0.5, -0.5, -0.5, 1));
+                manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(0.5, -0.5, -0.5, 1), new Vec4(0.5, -0.5, -0.5, 1));
                 //back surface
-                res[2] = manager.AddVertices(new Vec4(-0.5, 0.5, 0.5, 1), new Vec4(0.5, 0.5, 0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
-                res[3] = manager.AddVertices(new Vec4(-0.5, 0.5, 0.5, 1), new Vec4(0.5, -0.5, 0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
+                manager.AddVertices(new Vec4(-0.5, 0.5, 0.5, 1), new Vec4(0.5, 0.5, 0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
+                manager.AddVertices(new Vec4(-0.5, 0.5, 0.5, 1), new Vec4(0.5, -0.5, 0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
 
                 //left surface
-                res[4] = manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(-0.5, -0.5, -0.5, 1), new Vec4(-0.5, -0.5, 0.5, 1));
-                res[5] = manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(-0.5, -0.5, 0.5, 1));
+                manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(-0.5, -0.5, -0.5, 1), new Vec4(-0.5, -0.5, 0.5, 1));
+                manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(-0.5, -0.5, 0.5, 1));
                 //right surface
-                res[6] = manager.AddVertices(new Vec4(0.5, 0.5, -0.5, 1), new Vec4(0.5, -0.5, -0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
-                res[7] = manager.AddVertices(new Vec4(0.5, 0.5, -0.5, 1), new Vec4(0.5, 0.5, -0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
+                manager.AddVertices(new Vec4(0.5, 0.5, -0.5, 1), new Vec4(0.5, -0.5, -0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
+                manager.AddVertices(new Vec4(0.5, 0.5, -0.5, 1), new Vec4(0.5, 0.5, -0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
                 
                 //top surface
-                res[8] = manager.AddVertices(new Vec4(-0.5, -0.5, -0.5, 1), new Vec4(-0.5, -0.5, 0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
-                res[9] = manager.AddVertices(new Vec4(-0.5, -0.5, -0.5, 1), new Vec4(0.5, -0.5, 0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
+                 manager.AddVertices(new Vec4(-0.5, -0.5, -0.5, 1), new Vec4(-0.5, -0.5, 0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
+                 manager.AddVertices(new Vec4(-0.5, -0.5, -0.5, 1), new Vec4(0.5, -0.5, 0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
                 //bottom surface
-                res[10] = manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(-0.5, 0.5, 0.5, 1), new Vec4(0.5, 0.5, 0.5, 1));
-                res[11] = manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(0.5, 0.5, 0.5, 1), new Vec4(0.5, 0.5, 0.5, 1));
+                 manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(-0.5, 0.5, 0.5, 1), new Vec4(0.5, 0.5, 0.5, 1));
+                 manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(0.5, 0.5, 0.5, 1), new Vec4(0.5, 0.5, 0.5, 1));
             }
             else 
             {
-                res[0] = manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(0.5, 0.5, -0.5, 1));
-                res[1] = manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(-0.5, -0.5, -0.5, 1));
-                res[2] = manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(-0.5, 0.5, 0.5, 1));
+                manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(0.5, 0.5, -0.5, 1));
+                manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(-0.5, -0.5, -0.5, 1));
+                manager.AddVertices(new Vec4(-0.5, 0.5, -0.5, 1), new Vec4(-0.5, 0.5, 0.5, 1));
 
-                res[3] = manager.AddVertices(new Vec4(0.5, -0.5, -0.5, 1), new Vec4(0.5, 0.5, -0.5, 1));
-                res[4] = manager.AddVertices(new Vec4(0.5, -0.5, -0.5, 1), new Vec4(-0.5, -0.5, -0.5, 1));
-                res[5] = manager.AddVertices(new Vec4(0.5, -0.5, -0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
+                manager.AddVertices(new Vec4(0.5, -0.5, -0.5, 1), new Vec4(0.5, 0.5, -0.5, 1));
+                manager.AddVertices(new Vec4(0.5, -0.5, -0.5, 1), new Vec4(-0.5, -0.5, -0.5, 1));
+                manager.AddVertices(new Vec4(0.5, -0.5, -0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
 
-                res[6] = manager.AddVertices(new Vec4(-0.5, -0.5, 0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
-                res[7] = manager.AddVertices(new Vec4(-0.5, -0.5, 0.5, 1), new Vec4(-0.5, 0.5, 0.5, 1));
-                res[8] = manager.AddVertices(new Vec4(-0.5, -0.5, 0.5, 1), new Vec4(-0.5, -0.5, -0.5, 1));
+                manager.AddVertices(new Vec4(-0.5, -0.5, 0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
+                manager.AddVertices(new Vec4(-0.5, -0.5, 0.5, 1), new Vec4(-0.5, 0.5, 0.5, 1));
+                manager.AddVertices(new Vec4(-0.5, -0.5, 0.5, 1), new Vec4(-0.5, -0.5, -0.5, 1));
 
-                res[9] = manager.AddVertices(new Vec4(0.5, 0.5, 0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
-                res[10] = manager.AddVertices(new Vec4(0.5, 0.5, 0.5, 1), new Vec4(-0.5, 0.5, 0.5, 1));
-                res[11] = manager.AddVertices(new Vec4(0.5, 0.5, 0.5, 1), new Vec4(0.5, 0.5, -0.5, 1));
+                manager.AddVertices(new Vec4(0.5, 0.5, 0.5, 1), new Vec4(0.5, -0.5, 0.5, 1));
+                manager.AddVertices(new Vec4(0.5, 0.5, 0.5, 1), new Vec4(-0.5, 0.5, 0.5, 1));
+                manager.AddVertices(new Vec4(0.5, 0.5, 0.5, 1), new Vec4(0.5, 0.5, -0.5, 1));
             }
-
-            return res;
         }
         ///<summary>
         /// Constructs a pyramid with square base of height=1 and side length=1, center of base surface laying at (0,0,0)
         ///</summary>
-        public static GObject[] MakePyramid(bool fill, TMaker tmaker, GManager manager)
+        public static void MakePyramid(bool fill, GManager manager)
         {
-            GObject[] res = null;
             var tip = new Vec4(0,0,-1,1);
             if (fill)
             {
-                res = new GObject[6];
-
                 //base
-                res[0] = manager.AddVertices(new Vec4(-0.5, -0.5, 0, 1), new Vec4(-0.5, 0.5, 0, 1), new Vec4(0.5, 0.5, 0, 1));
-                res[1] = manager.AddVertices(new Vec4(-0.5, -0.5, 0, 1), new Vec4(-0.5, 0.5, 0, 1), new Vec4(0.5, 0.5, 0, 1));
+                manager.AddVertices(new Vec4(-0.5, -0.5, 0, 1), new Vec4(-0.5, 0.5, 0, 1), new Vec4(0.5, 0.5, 0, 1));
+                manager.AddVertices(new Vec4(-0.5, -0.5, 0, 1), new Vec4(-0.5, 0.5, 0, 1), new Vec4(0.5, 0.5, 0, 1));
 
-                res[2] = manager.AddVertices(new Vec4(-0.5, -0.5, 0, 1), new Vec4(0.5, -0.5, 0, 1), tip);
-                res[3] = manager.AddVertices(new Vec4(0.5, -0.5, 0, 1), new Vec4(0.5, 0.5, 0, 1), tip);
-                res[4] = manager.AddVertices(new Vec4(0.5, 0.5, 0, 1), new Vec4(-0.5, 0.5, 0, 1), tip);
-                res[5] = manager.AddVertices(new Vec4(-0.5, 0.5, 0, 1), new Vec4(-0.5, -0.5, 0, 1), tip);
+                manager.AddVertices(new Vec4(-0.5, -0.5, 0, 1), new Vec4(0.5, -0.5, 0, 1), tip);
+                manager.AddVertices(new Vec4(0.5, -0.5, 0, 1), new Vec4(0.5, 0.5, 0, 1), tip);
+                manager.AddVertices(new Vec4(0.5, 0.5, 0, 1), new Vec4(-0.5, 0.5, 0, 1), tip);
+                manager.AddVertices(new Vec4(-0.5, 0.5, 0, 1), new Vec4(-0.5, -0.5, 0, 1), tip);
             }
             else 
             {
-                res = new GObject[8];
+                manager.AddVertices(new Vec4(-0.5, -0.5, 0, 1), new Vec4(0.5, -0.5, 0, 1), tip);
+                manager.AddVertices(new Vec4(0.5, -0.5, 0, 1), new Vec4(0.5, 0.5, 0, 1), tip);
+                manager.AddVertices(new Vec4(0.5, 0.5, 0, 1), new Vec4(-0.5, 0.5, 0, 1), tip);
+                manager.AddVertices(new Vec4(-0.5, 0.5, 0, 1), new Vec4(-0.5, -0.5, 0, 1), tip);
                 
-                res[0] = manager.AddVertices(new Vec4(-0.5, -0.5, 0, 1), new Vec4(0.5, -0.5, 0, 1), tip);
-                res[1] = manager.AddVertices(new Vec4(0.5, -0.5, 0, 1), new Vec4(0.5, 0.5, 0, 1), tip);
-                res[2] = manager.AddVertices(new Vec4(0.5, 0.5, 0, 1), new Vec4(-0.5, 0.5, 0, 1), tip);
-                res[3] = manager.AddVertices(new Vec4(-0.5, 0.5, 0, 1), new Vec4(-0.5, -0.5, 0, 1), tip);
-                
-                res[4] = manager.AddVertices(new Vec4(-0.5, -0.5, 0, 1), tip);
-                res[5] = manager.AddVertices(new Vec4(0.5, -0.5, 0, 1), tip);
-                res[6] = manager.AddVertices(new Vec4(-0.5, 0.5, 0, 1), tip);
-                res[7] = manager.AddVertices(new Vec4(-0.5, 0.5, 0, 1), tip);
+                manager.AddVertices(new Vec4(-0.5, -0.5, 0, 1), tip);
+                manager.AddVertices(new Vec4(0.5, -0.5, 0, 1), tip);
+                manager.AddVertices(new Vec4(-0.5, 0.5, 0, 1), tip);
+                manager.AddVertices(new Vec4(-0.5, 0.5, 0, 1), tip);
             }
-            return res;
         }
 
         ///<summary>
         ///Constructs a cone of height=1 and radius=1. The center of the base lays at (0,0,0) and the tip at (0,0,-1)
         ///<param name="numberOfSplits">The number of times the sides are split into two. Value must be >= 2, wheres 2 would equal a pyramid</param>
         ///</summary>
-        public static GObject[] MakeCone(bool fill, int numberOfSplits, TMaker tmaker, GManager manager)
+        public static void MakeCone(bool fill, int numberOfSplits, GManager manager)
         {
             if (numberOfSplits < 2)
                 throw new ArgumentException("NumberOfSplits must be at least 2 or more", "numberOfSplits");
 
             int numSegments = 1 << numberOfSplits;
-            var res = new GObject[2 * numSegments];
             
             var tip = new Vec4(0,0,-1,1);
             var center = new Vec4(0,0,0,1);
@@ -131,29 +121,29 @@ namespace MiniGL
                     y = circle[i].Y;
                     nx = circle[i + 1].X;
                     ny = circle[i + 1].Y;
-                    res[i * 4] = manager.AddVertices(new Vec4(x, y, 0, 1), new Vec4(nx, ny, 0, 1), center);
-                    res[i * 4 + 1] = manager.AddVertices(new Vec4(-x, y, 0, 1), new Vec4(-nx, ny, 0, 1), center);
-                    res[i * 4 + 2] = manager.AddVertices(new Vec4(-x, -y, 0, 1), new Vec4(-nx, -ny, 0, 1), center);
-                    res[i * 4 + 3] = manager.AddVertices(new Vec4(x, -y, 0, 1), new Vec4(nx, -ny, 0, 1), center);
+                    manager.AddVertices(new Vec4(x, y, 0, 1), new Vec4(nx, ny, 0, 1), center);
+                    manager.AddVertices(new Vec4(-x, y, 0, 1), new Vec4(-nx, ny, 0, 1), center);
+                    manager.AddVertices(new Vec4(-x, -y, 0, 1), new Vec4(-nx, -ny, 0, 1), center);
+                    manager.AddVertices(new Vec4(x, -y, 0, 1), new Vec4(nx, -ny, 0, 1), center);
                     
-                    res[i * 8] = manager.AddVertices(new Vec4(x, y, 0, 1), new Vec4(nx, ny, 0, 1), tip);
-                    res[i * 8 + 1] = manager.AddVertices(new Vec4(-x, y, 0, 1), new Vec4(-nx, ny, 0, 1), tip);
-                    res[i * 8 + 2] = manager.AddVertices(new Vec4(-x, -y, 0, 1), new Vec4(-nx, -ny, 0, 1), tip);
-                    res[i * 8 + 3] = manager.AddVertices(new Vec4(x, -y, 0, 1), new Vec4(nx, -ny, 0, 1), tip);
+                    manager.AddVertices(new Vec4(x, y, 0, 1), new Vec4(nx, ny, 0, 1), tip);
+                    manager.AddVertices(new Vec4(-x, y, 0, 1), new Vec4(-nx, ny, 0, 1), tip);
+                    manager.AddVertices(new Vec4(-x, -y, 0, 1), new Vec4(-nx, -ny, 0, 1), tip);
+                    manager.AddVertices(new Vec4(x, -y, 0, 1), new Vec4(nx, -ny, 0, 1), tip);
                 }
                 x = nx;
                 y = ny;
                 nx = 0;
                 ny = 1;
-                res[i * 4] = manager.AddVertices(new Vec4(x, y, 0, 1), new Vec4(nx, ny, 0, 1), center);
-                res[i * 4 + 1] = manager.AddVertices(new Vec4(-x, y, 0, 1), new Vec4(-nx, ny, 0, 1), center);
-                res[i * 4 + 2] = manager.AddVertices(new Vec4(-x, -y, 0, 1), new Vec4(-nx, -ny, 0, 1), center);
-                res[i * 4 + 3] = manager.AddVertices(new Vec4(x, -y, 0, 1), new Vec4(nx, -ny, 0, 1), center);
+                manager.AddVertices(new Vec4(x, y, 0, 1), new Vec4(nx, ny, 0, 1), center);
+                manager.AddVertices(new Vec4(-x, y, 0, 1), new Vec4(-nx, ny, 0, 1), center);
+                manager.AddVertices(new Vec4(-x, -y, 0, 1), new Vec4(-nx, -ny, 0, 1), center);
+                manager.AddVertices(new Vec4(x, -y, 0, 1), new Vec4(nx, -ny, 0, 1), center);
                 
-                res[i * 8] = manager.AddVertices(new Vec4(x, y, 0, 1), new Vec4(nx, ny, 0, 1), tip);
-                res[i * 8 + 1] = manager.AddVertices(new Vec4(-x, y, 0, 1), new Vec4(-nx, ny, 0, 1), tip);
-                res[i * 8 + 2] = manager.AddVertices(new Vec4(-x, -y, 0, 1), new Vec4(-nx, -ny, 0, 1), tip);
-                res[i * 8 + 3] = manager.AddVertices(new Vec4(x, -y, 0, 1), new Vec4(nx, -ny, 0, 1), tip);
+                manager.AddVertices(new Vec4(x, y, 0, 1), new Vec4(nx, ny, 0, 1), tip);
+                manager.AddVertices(new Vec4(-x, y, 0, 1), new Vec4(-nx, ny, 0, 1), tip);
+                manager.AddVertices(new Vec4(-x, -y, 0, 1), new Vec4(-nx, -ny, 0, 1), tip);
+                manager.AddVertices(new Vec4(x, -y, 0, 1), new Vec4(nx, -ny, 0, 1), tip);
 
             }
             else 
@@ -165,35 +155,34 @@ namespace MiniGL
                     y = circle[i].Y;
                     nx = circle[i + 1].X;
                     ny = circle[i + 1].Y;
-                    res[i * 4] = manager.AddVertices(new Vec4(x, y, 0, 1), new Vec4(nx, ny, 0, 1));
-                    res[i * 4 + 1] = manager.AddVertices(new Vec4(-x, y, 0, 1), new Vec4(-nx, ny, 0, 1));
-                    res[i * 4 + 2] = manager.AddVertices(new Vec4(-x, -y, 0, 1), new Vec4(-nx, -ny, 0, 1));
-                    res[i * 4 + 3] = manager.AddVertices(new Vec4(x, -y, 0, 1), new Vec4(nx, -ny, 0, 1));
+                    manager.AddVertices(new Vec4(x, y, 0, 1), new Vec4(nx, ny, 0, 1));
+                    manager.AddVertices(new Vec4(-x, y, 0, 1), new Vec4(-nx, ny, 0, 1));
+                    manager.AddVertices(new Vec4(-x, -y, 0, 1), new Vec4(-nx, -ny, 0, 1));
+                    manager.AddVertices(new Vec4(x, -y, 0, 1), new Vec4(nx, -ny, 0, 1));
                     
-                    res[i * 8] = manager.AddVertices(new Vec4(x, y, 0, 1), tip);
-                    res[i * 8 + 1] = manager.AddVertices(new Vec4(-x, y, 0, 1),tip);
-                    res[i * 8 + 2] = manager.AddVertices(new Vec4(-x, -y, 0, 1), tip);
-                    res[i * 8 + 3] = manager.AddVertices(new Vec4(x, -y, 0, 1), tip);
+                    manager.AddVertices(new Vec4(x, y, 0, 1), tip);
+                    manager.AddVertices(new Vec4(-x, y, 0, 1),tip);
+                    manager.AddVertices(new Vec4(-x, -y, 0, 1), tip);
+                    manager.AddVertices(new Vec4(x, -y, 0, 1), tip);
                 }
 
                 x = nx;
                 y = ny;
                 nx = 0;
                 ny = 1;
-                res[i * 4] = manager.AddVertices(new Vec4(x, y, 0, 1), new Vec4(nx, ny, 0, 1));
-                res[i * 4 + 1] = manager.AddVertices(new Vec4(-x, y, 0, 1), new Vec4(-nx, ny, 0, 1));
-                res[i * 4 + 2] = manager.AddVertices(new Vec4(-x, -y, 0, 1), new Vec4(-nx, -ny, 0, 1));
-                res[i * 4 + 3] = manager.AddVertices(new Vec4(x, -y, 0, 1), new Vec4(nx, -ny, 0, 1));
+                manager.AddVertices(new Vec4(x, y, 0, 1), new Vec4(nx, ny, 0, 1));
+                manager.AddVertices(new Vec4(-x, y, 0, 1), new Vec4(-nx, ny, 0, 1));
+                manager.AddVertices(new Vec4(-x, -y, 0, 1), new Vec4(-nx, -ny, 0, 1));
+                manager.AddVertices(new Vec4(x, -y, 0, 1), new Vec4(nx, -ny, 0, 1));
                 
-                res[i * 8] = manager.AddVertices(new Vec4(x, y, 0, 1), tip);
-                res[i * 8 + 1] = manager.AddVertices(new Vec4(-x, y, 0, 1),tip);
-                res[i * 8 + 2] = manager.AddVertices(new Vec4(-x, -y, 0, 1), tip);
-                res[i * 8 + 3] = manager.AddVertices(new Vec4(x, -y, 0, 1), tip);
+                manager.AddVertices(new Vec4(x, y, 0, 1), tip);
+                manager.AddVertices(new Vec4(-x, y, 0, 1),tip);
+                manager.AddVertices(new Vec4(-x, -y, 0, 1), tip);
+                manager.AddVertices(new Vec4(x, -y, 0, 1), tip);
             }
-            return res;
         }
 
-        public static GObject[] MakeSphere(int numberOfSplits, bool fill, TMaker tmaker, GManager manager)
+        public static GObject[] MakeSphere(int numberOfSplits, bool fill, GManager manager)
         {
             //constructs a icosahedron and splits the triangles into smaller ones numberOfSplits times
             
